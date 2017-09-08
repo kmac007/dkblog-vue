bdaa427c825bc2e406f0c8fefd7adcdae937fe20
kmac007 <robbchan@foxmail.com>
2017-09-08 10:20:59 +0800

    连接数据库

--- server/db/db.js
+++ server/db/db.js
@@ -1,35 +1,45 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const sha1 = require('sha1')
-const rand = require('csprng')
 
-const UserSchema = new Schema(
-  {
-    name: String,
-    password: String
+mongoose.connect(
+  'mongodb://127.0.0.1/dk-blog',
+  { useMongoClient: true },
+  err => {
+    if (!err) {
+      console.log('The database has connected.')
+    } else {
+      console.log('Database connection error.')
+    }
   }
 )
 
-const ArticleSchema = new Schema(
-  {
-    aid: { type: Number, index: { unique: true } },
-    title: String,
-    content: String,
-    tags: [String],
-    date: Date,
-    isPublish: Boolean,
-    comment_n: Number
-  }
-)
+const UserSchema = new Schema({
+  name: String,
+  password: String
+})
 
-const CommentSchema = new Schema(
-  {
-    name: String,
-    email: String,
-    content: String,
-    articleId: Number,
-    date: Date
-  }
-)
+const ArticleSchema = new Schema({
+  aid: { type: Number, index: { unique: true } },
+  title: String,
+  content: String,
+  tags: [String],
+  date: Date,
+  isPublish: Boolean,
+  comment_n: Number
+})
 
-const 
+const CommentSchema = new Schema({
+  name: String,
+  email: String,
+  content: String,
+  articleId: Number,
+  date: Date
+})
+
+const Models = {
+  User: mongoose.model('User', UserSchema),
+  Article: mongoose.model('Article', ArticleSchema),
+  Comment: mongoose.model('Comment', CommentSchema)
+}
+
+module.exports = Models
